404d37ca40e2a2da4e658469e7e4c6e80c3e7f38
gabriel-p-Amaral <gabrielportodoamaral@gmail.com>
2021-11-14 11:07:01 -0300

    Adding code

---
+++ DescobrirIdade.js
@@ -0,0 +1,15 @@
+/**
+ * Você deve agora descobrir a idade do usuário
+ * para isso é recomendado que você use o seguinte calculo
+ * 
+ * Crie uma variável com o ano atual
+ * Crie uma variável com o ano de nascimento
+ * Crie uma variável com o resultado e armazena nessa variavel o ano atual - ano de nascimento
+ * 
+ * Exemplo:
+ */
+
+let a = 10
+let b = 20
+let result = b - a
+console.log(result)
